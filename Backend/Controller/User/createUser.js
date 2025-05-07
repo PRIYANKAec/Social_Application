4a80410c2120ce895d9524c6a51f73043faefac1
PRIYANKAec <priyankaa261103@gmail.com>
2025-05-07 09:41:16 +0530

    Implemented validation on data using yup[ADD]

--- Backend/Controller/User/createUser.js
+++ Backend/Controller/User/createUser.js
@@ -1,10 +1,24 @@
 const bcrypt = require('bcrypt');
+const yup = require('yup');
 
 const User = require('../../models/userModel');
 const generateToken = require('../../utils/jwtHash');
 
+const userSchema = yup.object().shape({
+firstName: yup.string().required().max(50),
+lastName: yup.string().required().max(50),
+username: yup.string().required('UserName is required'),
+email:yup.string().email('Invalid email format').required('Email is required'),
+password: yup.string().required('Password is required').min(6, 'Password should be atleast 6 characters'),
+})
+
+
 const createUser = async (req, res) => {
     try {
+
+    // Validate request body using Yup
+        await userSchema.validate(req.body);
+
         const { firstName, lastName, username, email, password } = req.body;
 
         // Validate required fields
@@ -29,6 +43,9 @@ const createUser = async (req, res) => {
         res.status(201).json({ message: "User created successfully.", user: newUser, token : token });
     } catch (error) {
         console.error("Error creating user:", error);
+        if (error.name === 'ValidationError') {
+            return res.status(400).json({ error: error.message });
+        }
         res.status(500).json({ error: "An error occurred while creating the user." });
     }
 };
