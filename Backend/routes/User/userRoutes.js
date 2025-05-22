f5696f07516ddc6c7cc2bdd10a049c3617163378
PRIYANKAec <priyankaa261103@gmail.com>
2025-05-22 20:12:06 +0530

    Refactored user route endpoints for consistency and clarity[FIX]

--- Backend/routes/User/userRoutes.js
+++ Backend/routes/User/userRoutes.js
@@ -7,11 +7,11 @@ const signIn = require('../../Controller/User/signIn');
 const updateUser = require('../../Controller/User/updateUser');
 const deleteUser = require('../../Controller/User/deleteUser');
 
-router.post('/create/register', createUser);
-router.get('/user/signIn', signIn);
+router.post('/register/user', createUser);
+router.get('/signIn/user', signIn);
 
-router.put('/user/update', authenticateToken, updateUser);
+router.put('/update/user', authenticateToken, updateUser);
 
-router.delete('/user/delete', authenticateToken, deleteUser);
+router.delete('/delete/user', authenticateToken, deleteUser);
 
 module.exports = router;
